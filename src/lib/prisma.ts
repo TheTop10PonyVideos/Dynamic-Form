e9cbab884db5caa14fb43ee59096f9eaef4b6eab
Brambles-c <gael.alejos05@gmail.com>
2026-07-18 22:16:14 -0700

    Package updates, remove dynamic labels

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,28 +1,10 @@
 // A file creating and ensuring only a single connection existing to the database between reloads made when ran using `next dev`
-
 import { PrismaClient } from "@/generated/prisma";
-import { labels } from "./labels";
-import { Flag } from "./types";
 
-/**
- * Create a new db connection while ensuring the label_config table is initialized with the default values defined in labels.ts.
- * If it has been, then initialize the runtime values with the contents of that table to use throughout the app
- */
-async function initConnection() {
-    const prisma = new PrismaClient()
-    const present = await prisma.label_config.findMany() as Flag[]
-
-    if (!present.length) {
-        await prisma.label_config.createMany({
-            data: Object.values(labels)
-        })
-    }
-
-    return prisma
-}
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || await initConnection()
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production")
+    globalForPrisma.prisma = prisma
--- src/lib/queries/etc.ts
+++ src/lib/queries/etc.ts
@@ -5,28 +5,17 @@ import { adjustDate } from "../util";
 
 export async function getAllData() {
     const [
-        users, video_metadata, ballot_item, playlist, playlist_item, label_config, manual_label
+        users, video_metadata, ballot_item, playlist, playlist_item, manual_label
     ] = await Promise.all([
         prisma.user.findMany(),
         prisma.video_metadata.findMany(),
         prisma.ballot_item.findMany(),
         prisma.playlist.findMany(),
         prisma.playlist_item.findMany(),
-        prisma.label_config.findMany(),
         prisma.manual_label.findMany()
     ])
 
-    return { users, video_metadata, ballot_item, label_config, manual_label, playlist, playlist_item }
-}
-
-
-export function setLabelConfigs(new_configs: Flag[]) {
-    return prisma.$transaction(
-        new_configs.map(config => prisma.label_config.update({
-            where: { trigger: config.trigger },
-            data: config
-        }))
-    )
+    return { users, video_metadata, ballot_item, manual_label, playlist, playlist_item }
 }
 
 
@@ -47,9 +36,4 @@ export async function getPool() {
             votes: v._count.ballot_item
         }
     })
-}
-
-
-export function getLabelConfigs() {
-    return prisma.label_config.findMany() as Promise<Flag[]>
 }
